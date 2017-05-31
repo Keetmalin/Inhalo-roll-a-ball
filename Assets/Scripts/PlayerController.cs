e86df7b27b69b1cdfdbb3be781a7421f81de9459
Keet Sugathadasa <keetmalin.13@cse.mrt.ac.lk>
2017-05-31 20:18:43 +0530

    Adding initial project files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody rb;
+	private int count;
+	public Text healthText;
+
+	public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+	
+		healthText.text = "";
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetText ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//physics properties
+	void FixedUpdate(){
+
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (0.0f, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.gameObject.CompareTag ("Coin")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetText ();
+		}
+	
+	}
+
+	void SetText(){
+	
+		scoreText.text = "Score: " + (count*10).ToString ();
+	}
+
+}
